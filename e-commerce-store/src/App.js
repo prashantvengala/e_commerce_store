a32d589399bdbbe713812323dbcb2bf6e1106e3d
prashanth <vengalaprashanth24@gmail.com>
2021-07-13 08:00:42 +0530

    added api key from commerceJS

--- e-commerce-store/src/App.js
+++ e-commerce-store/src/App.js
@@ -1,6 +1,21 @@
+import { useState, useEffect } from "react";
 import { Products, Navbar } from "./Components";
+import { commerce } from "./lib/commerce";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
   return (
     <div className="App">
       <Navbar />
