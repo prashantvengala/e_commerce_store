c6be6312316a9db240eb5bd52560d49fd9a85540
prashanth <vengalaprashanth24@gmail.com>
2021-07-14 06:48:52 +0530

    added cartItem to display the cart

--- e-commerce-store/src/Components/Cart/CartItem/CarItem.jsx
+++ e-commerce-store/src/Components/Cart/CartItem/CarItem.jsx
@@ -12,7 +12,48 @@ import useStyles from "./style";
 
 const CarItem = () => {
   const classes = useStyles();
-  return <div></div>;
+  return (
+    <Card className="cart-item">
+      <CardMedia
+        image={item.media.source}
+        alt={item.name}
+        className={classes.media}
+      />
+      <CardContent className={classes.cardContent}>
+        <Typography variant="h4">{item.name}</Typography>
+        <Typography variant="h5">
+          {item.line_total.formatted_with_symbol}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <div className={classes.buttons}>
+          <Button
+            type="button"
+            size="small"
+            onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}
+          >
+            -
+          </Button>
+          <Typography>&nbsp;{item.quantity}&nbsp;</Typography>
+          <Button
+            type="button"
+            size="small"
+            onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}
+          >
+            +
+          </Button>
+        </div>
+        <Button
+          variant="contained"
+          type="button"
+          color="secondary"
+          onClick={() => handleRemoveFromCart(item.id)}
+        >
+          Remove
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default CarItem;
