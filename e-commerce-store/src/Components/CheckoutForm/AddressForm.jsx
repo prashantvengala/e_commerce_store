dd974083656ffba1c7e79657ba92c62344761ee1
prashanth <vengalaprashanth24@gmail.com>
2021-07-16 23:56:07 +0530

    checkout form updated

--- e-commerce-store/src/Components/CheckoutForm/AddressForm.jsx
+++ e-commerce-store/src/Components/CheckoutForm/AddressForm.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import {
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import { useForm, FormProvider } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { commerce } from "../../lib/commerce";
+import FormInput from "./FormInput";
 
 const AddressForm = () => {
-    return (
-        <div>
-            address
-        </div>
-    )
-}
+  const [shippingCountries, setShippingCountries] = useState([]);
+  const [shippingCountry, setShippingCountry] = useState("");
+  const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
+  const [shippingSubdivision, setShippingSubdivision] = useState("");
+  const [shippingOptions, setShippingOptions] = useState([]);
+  const [shippingOption, setShippingOption] = useState("");
+  const methods = useForm();
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping address
+      </Typography>
+      <FormProvider {...methods}>
+        <form>
+          <Grid container spacing={3}>
+            <FormInput required name="firstName" label="First name" />
+            <FormInput required name="lastName" label="Last name" />
+            <FormInput required name="address1" label="Address line 1" />
+            <FormInput required name="email" label="Email" />
+            <FormInput required name="city" label="City" />
+            <FormInput required name="zip" label="Zip / Postal code" />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <InputLabel>Shipping </InputLabel>
+          </Grid>
+        </form>
+      </FormProvider>
+    </>
+  );
+};
 
-export default AddressForm
+export default AddressForm;
